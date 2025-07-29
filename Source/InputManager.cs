c1ee2e01fabbfcb2cbcb91ece6bc39df0eb14cee
Orion Moonclaw <killer@fawx.news>
2025-07-29 03:43:15 +0200

    fix formatting oopsie

--- Source/InputManager.cs
+++ Source/InputManager.cs
@@ -7,33 +7,33 @@ namespace Renderite.Godot.Source;
 
 public partial class InputManager : Node
 {
-	public static InputManager Instance;
-	private HashSet<Shared.Key> _heldKeys = new HashSet<Shared.Key>();
+    public static InputManager Instance;
+    private HashSet<Shared.Key> _heldKeys = new HashSet<Shared.Key>();
 
-	public override void _Ready()
-	{
-		base._Ready();
-		Instance = this;
-	}
+    public override void _Ready()
+    {
+        base._Ready();
+        Instance = this;
+    }
 
-	public override void _Input(InputEvent @event)
-	{
-		if (@event is InputEventKey keyEvent)
-		{
-			if (keyEvent.Pressed)
-				_heldKeys.Add(keyEvent.Keycode.ToRenderite(keyEvent.Location));
-			else
-				_heldKeys.Remove(keyEvent.Keycode.ToRenderite(keyEvent.Location));
-		}
-	}
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventKey keyEvent)
+        {
+            if (keyEvent.Pressed)
+                _heldKeys.Add(keyEvent.Keycode.ToRenderite(keyEvent.Location));
+            else
+                _heldKeys.Remove(keyEvent.Keycode.ToRenderite(keyEvent.Location));
+        }
+    }
 
-	public InputState GetInputState() => new()
-	{
-		keyboard = new KeyboardState
-		{
-			heldKeys = _heldKeys,
-			// TODO: typeDelta (if I'm thinking about this correctly it's used for actual typing)
-		}
-		// TODO: mouse and window state
-	};
+    public InputState GetInputState() => new()
+    {
+        keyboard = new KeyboardState
+        {
+            heldKeys = _heldKeys,
+            // TODO: typeDelta (if I'm thinking about this correctly it's used for actual typing)
+        }
+        // TODO: mouse and window state
+    };
 }
