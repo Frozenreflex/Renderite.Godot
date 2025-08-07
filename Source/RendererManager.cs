6a5c1ef31c5e6ab870b04f63aee20f7b2dd52caa
Orion Moonclaw <killer@fawx.news>
2025-08-08 00:38:20 +0200

    bootstrap Resonite.dll from the renderer side

--- Source/RendererManager.cs
+++ Source/RendererManager.cs
@@ -5,13 +5,19 @@ using Godot;
 using Renderite.Godot.Source.Helpers;
 using Renderite.Shared;
 using Renderite.Godot.Source.SharedMemory;
-using System.IO;
+using Cloudtoid.Interprocess;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
 
 namespace Renderite.Godot.Source;
 
 public partial class RendererManager : Node
 {
     public static RendererManager Instance;
+
+    private Process _resoniteProcess;
+    private bool _resoniteQuit;
 
     public Dictionary<int, RenderSpace> Spaces = new();
     public AssetManager AssetManager = new();
@@ -30,54 +36,81 @@ public partial class RendererManager : Node
     {
         base._Ready();
         Instance = this;
-
-        string queueName = null;
-        var queueCapacity = 8388608;
-
         var args = OS.GetCmdlineArgs();
+
+        GD.Print("Starting...");
+        const string safeChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var shmPrefix = new string(Enumerable.Range(0, 16)
+            .Select(_ => safeChars[Random.Shared.Next(safeChars.Length)])
+            .ToArray());
+        var bootstrapperIn = new QueueFactory().CreateSubscriber(new QueueOptions(shmPrefix + ".bootstrapper_in", 8192L, deleteOnDispose: true));
+        var bootstrapperOut = new QueueFactory().CreatePublisher(new QueueOptions(shmPrefix + ".bootstrapper_out", 8192L, deleteOnDispose: true));
+        GD.Print("Bootstrapper queue created.");
+
+        var resonitePath = OS.HasFeature("windows") ?
+                           @"C:\Program Files (x86)\Steam\steamapps\common\Resonite\" :
+                           System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile), ".local", "share", "Steam", "steamapps", "common", "Resonite");
+        var executable = "dotnet";
 
         for (var i = 0; i < args.Length; i++)
         {
             var arg = args[i].ToLower();
-            if (arg.Contains("queuename"))
+            if (arg.Contains("resonitepath"))
             {
                 var next = args[i + 1];
                 i++;
-                queueName = next;
+                resonitePath = next;
             }
-            else if (arg.Contains("queuecapacity"))
+            else if (arg.Contains("executable"))
             {
                 var next = args[i + 1];
                 i++;
-                queueCapacity = int.Parse(next);
+                executable = next;
             }
         }
+        GD.Print($"Resonite path: {resonitePath}");
 
-        if (queueName is null)
+        var dllPath = System.IO.Path.Combine(resonitePath, "Resonite.dll");
+        var resoniteArgs = args.SkipWhile(arg => arg.ToLower() != "--resoniteargs").Skip(1).ToArray().Join(" ");
+        var startInfo = new ProcessStartInfo
         {
-            const string shmPath = "/dev/shm";
-            const string prefix = "sem.ct.ip.";
-            const string suffix = "PrimaryA";
+            FileName = executable,
+            Arguments = $"{dllPath} -shmprefix {shmPrefix} {resoniteArgs}",
+            WorkingDirectory = resonitePath,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true
+        };
 
-            var files = Directory.EnumerateFiles(shmPath)
-                .Where(f => Path.GetFileName(f).StartsWith(prefix) && f.EndsWith(suffix))
-                .OrderByDescending(f => File.GetLastWriteTime(f))
-                .ToList();
+        _resoniteProcess = new Process
+        {
+            StartInfo = startInfo,
+            EnableRaisingEvents = true
+        };
 
-            if (files.Count > 0)
-            {
-                queueName = Path.GetFileName(files[0])[prefix.Length..^suffix.Length];
-                GD.Print($"Detected queueName from file: {files[0]}");
-            }
-            else
-            {
-                GD.PrintErr("No queue name specified!");
-                GetTree().Quit();
-                return;
-            }
-        }
+        _resoniteProcess.OutputDataReceived += (sender, e) => { if (e.Data != null) GD.Print($"[Resonite] {e.Data}"); };
+        _resoniteProcess.ErrorDataReceived += (sender, e) => { if (e.Data != null) GD.PrintErr($"[Resonite] {e.Data}"); };
+        _resoniteProcess.Exited += (sender, e) =>
+        {
+            _resoniteQuit = true;
+            GD.Print($"Resonite process quit with code {_resoniteProcess.ExitCode}, we're going down too.");
+            GetTree().CallDeferred("quit");
+        };
+        GetTree().AutoAcceptQuit = false;
 
-        GD.Print($"Connecting to {queueName} (capacity: {queueCapacity})");
+        GD.Print($"Starting Resonite with args {startInfo.Arguments}");
+        _resoniteProcess.Start();
+        _resoniteProcess.BeginOutputReadLine();
+        _resoniteProcess.BeginErrorReadLine();
+
+        var queueArgs = Encoding.UTF8.GetString(bootstrapperIn.Dequeue(CancellationToken.None).Span).Split(' ');
+        var queueName = queueArgs[1];
+        var queueCapacity = long.Parse(queueArgs[3]);
+
+        bootstrapperOut.TryEnqueue(Encoding.UTF8.GetBytes("RENDERITE_STARTED:" + System.Environment.ProcessId));
+
+        GD.Print($"Received queue info, connecting to {queueName} (capacity: {queueCapacity})");
         PrimaryMessagingManager = new MessagingManager(PackerMemoryPool.Instance);
         PrimaryMessagingManager.CommandHandler = HandleRenderCommand;
         PrimaryMessagingManager.FailureHandler = HandleMessagingFailure;
@@ -90,6 +123,14 @@ public partial class RendererManager : Node
         BackgroundMessagingManager.Connect(queueName + "Background", isAuthority: false, queueCapacity);
         GD.Print("Connected!");
     }
+    public override void _Notification(int what)
+    {
+        if (what == NotificationWMCloseRequest)
+        {
+            GD.Print("Requesting shutdown...");
+            PrimaryMessagingManager.SendCommand(new RendererShutdownRequest());
+        }
+    }
     public override void _Process(double delta)
     {
         base._Process(delta);
@@ -99,12 +140,14 @@ public partial class RendererManager : Node
         var frameStartData = new FrameStartData
         {
             lastFrameIndex = LastFrameIndex,
-            inputs = InputManager.Instance.GetInputState()
+            inputs = InputManager.Instance.GetInputState(),
         };
         PrimaryMessagingManager.SendCommand(frameStartData);
 
         while (_frameData == null)
         {
+            if (_resoniteQuit)
+                return;
         } // Maybe don't tight loop here?
 
         if (_frameData != null)
