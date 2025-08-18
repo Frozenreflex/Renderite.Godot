a0adf221f82d1d90c3130c09759a0b08db78f11b
Orion Moonclaw <killer@fawx.news>
2025-08-18 22:09:42 +0200

    fix lights pointing in the opposite direction

--- Source/Space/Scene/LightInstance.cs
+++ Source/Space/Scene/LightInstance.cs
@@ -6,4 +6,11 @@ public class LightInstance : AssetSceneInstance
 {
     public Rid LightRid;
     public RenderingServer.LightType Type = (RenderingServer.LightType)(-1);
+    
+    protected override void BaseOnGlobalTransformChanged(TransformNode obj)
+    {
+        var transform = obj.GlobalTransform;
+        transform.Basis *= new Basis(new Quaternion(Vector3.Right, Mathf.Pi));
+        RenderingServer.InstanceSetTransform(InstanceRid, transform);
+    }
 }
